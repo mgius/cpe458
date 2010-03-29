e40c40c80327f1d554e864c768cde6a50f276547
Mark Gius <mgius7096@gmail.com>
2010-03-29 15:45:59 -0700

    I'm guessing I need to denote naughtiness in the random result function

--- lab1/probability.fs
+++ lab1/probability.fs
@@ -6,5 +6,14 @@ let randomReturn =
         | x when x < 0.65 -> 2
         | _ -> 3
 
+let randomReturn2 = 
+    let x = System.Random().NextDouble()
+    if x < 0.1 then 0
+    elif x < 0.6 then 1
+    elif x < 0.65 then 2
+    else 3
+
+
+
 for i = 1 to 10 do
-    printfn "%d" randomReturn
+    printfn "%d" randomReturn2
