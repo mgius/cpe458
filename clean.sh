65961034667a14e200504016103b12318d9c770a
Mark Gius <mgius7096@gmail.com>
2010-03-29 12:56:00 -0700

    Since I fixed up the scripts, don't need to do this anymore

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 ## Stupid F# is pulling a ton of crap into these folders.  Clean em out.
 
-find . -iname '*.dll' -o -iname '*.exe' -print -exec rm -f {} \;
+find . -iname '*.exe' -print -delete
