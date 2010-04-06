ec5708f4f74cf98fea8fee7ffb792497f5a9ea13
Mark <mgius7096@gmail.com>
2010-04-05 20:24:30 -0700

    Basic game complete

--- program1/chutes.fs
+++ program1/chutes.fs
@@ -1,5 +1,9 @@
 #light
 
+let die = System.Random()
+
+let roll () = die.Next(1,7)
+
 let gameBoard pos = 
     match pos with
         | 1 -> 38
@@ -22,3 +26,11 @@ let gameBoard pos =
         | 96 -> 75
         | 98 -> 78
         | _ -> pos
+
+let rec rungame onePos twoPos whoseTurn =
+    match gameBoard (onePos + (roll ())) with
+        | newPos when newPos > 100 ->
+            whoseTurn % 2 + 1 |> printfn "Player %d has won!"
+        | newPos -> rungame twoPos newPos (whoseTurn + 1)
+
+rungame 0 0 0
