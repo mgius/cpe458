534d9aded1dcd7d77d60db421effc1bf256b6255
Mark Gius <mgius7096@gmail.com>
2010-04-21 10:00:32 -0700

    some work on lab2.  Seq.sort isn't working like I think it should

--- lab2/lab2.fs
+++ lab2/lab2.fs
@@ -20,16 +20,37 @@ let oneStepInner (rGen : System.Random) input =
 let oneStep = oneStepInner r
 let oneStepTester = oneStepInner rTest
 
+let nStepsInner innerOneStep d i  =
+    List.fold (fun acc x -> (innerOneStep acc)) d [for j in 1..i -> j]
 
-(differ f1 1.0) |> printfn "%.9f"
-[<Fact>]
-let f1Test () =
-    should equal (f1 5.0) 89.0
+let nSteps = nStepsInner oneStep
+let nStepsTester = nStepsInner oneStepTester
 
-[<Fact>]
-let differTest () =
-    should equal (differ f1 1.0) 0.000000003
+(nStepsTester 1.0 5) |> printfn "%f"
 
+let seqn20 = seq { for i in 1..1000 -> (nSteps 1.0 20) }
+let seqn20counted = Seq.countBy (fun elem -> elem) seqn20
+
+let seqn40 = seq { for i in 1..1000 -> (nSteps 1.0 40) }
+let seqn40counted = Seq.sort (Seq.countBy (fun elem -> elem) seqn40)
+
+let printSeq seq1 = Seq.iter (printf "%A \n") seq1; printfn ""
+
+printSeq seqn20
+printfn "\n\n"
+printSeq seqn40
+
+
+
+//(differ f1 1.0) |> printfn "%.9f"
+//[<Fact>]
+//let f1Test () =
+//    should equal (f1 5.0) 89.0
+//
+//[<Fact>]
+//let differTest () =
+//    should equal (differ f1 1.0) 0.000000006
+//
 //[<Fact>]
 //let oneStepTests () =
 //    should equal (oneStepTester 1000.0) 1010.0
