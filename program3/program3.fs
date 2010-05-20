e29690b2ee48a38325baf74979f2d6f8f9573c81
Mark Gius <mgius7096@gmail.com>
2010-05-19 22:22:47 -0700

    My head hurts

--- program3/program3.fs
+++ program3/program3.fs
@@ -1,16 +1,19 @@
 module program3 
 
 type term = (double * double)
-type eqn = term list
+type poly = term list
 
-let deriv (terms : eqn) =
+let deriv (terms : poly) =
    let singlederiv (a, b) =
       if b = 0.0 then [] else [(a * b, b - 1.0)]
-   let rec loop (acc : eqn) = function
+   let rec loop (acc : poly) = function
       | [] -> acc
       | hd :: tl -> 
          loop (acc @ (singlederiv hd)) tl
    loop [] terms
 
-let evalPoly (poly : eqn) x =
-   List.fold (fun acc (a,b) -> acc + a * (x ** b)) 0.0 poly
+let evalPoly (eqn : poly) x =
+   List.fold (fun acc (a,b) -> acc + a * (x ** b)) 0.0 eqn
+
+let findZeros (eqn, x) =
+   
